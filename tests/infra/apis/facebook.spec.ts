a7d85882ca34a0cec46e4a4f7a24971da6e76ead
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-02-05 16:30:39 -0300

    refactor: move production code to their own files

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -1,39 +1,7 @@
-import { LoadFacebookUserApi } from '@/data/contracts/apis'
+import { FacebookApi } from '@/infra/apis/facebook'
+import { HttpGetClient } from '@/infra/http'
 
 import { mock, MockProxy } from 'jest-mock-extended'
-
-class FacebookApi implements LoadFacebookUserApi {
-  private readonly baseUrl: string = 'https://graph.facebook.com'
-
-  constructor (
-    private readonly httpClient: HttpGetClient,
-    private readonly clientId: string,
-    private readonly clientSecret: string
-  ) {}
-
-  async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
-    await this.httpClient.get({
-      url: `${this.baseUrl}/oauth/access_token`,
-      params: {
-        client_id: this.clientId,
-        client_secret: this.clientSecret,
-        grant_type: 'any_grant_type'
-      }
-    })
-    return undefined
-  }
-}
-
-interface HttpGetClient {
-  get: (params: HttpGetClient.Params) => Promise<void>
-}
-
-namespace HttpGetClient {
-  export type Params = {
-    url: string
-    params: object
-  }
-}
 
 describe('FacebookApi', () => {
   let clientId: string
