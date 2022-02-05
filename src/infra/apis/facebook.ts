d475b6b5835159f8407a2ac683bfcd4b593e0915
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-02-05 18:26:14 -0300

    refactor: code improvements on FacebookApi implementation

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -1,6 +1,22 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { HttpGetClient } from '@/infra/http'
 
+type AppToken = {
+  access_token: string
+}
+
+type DebugToken = {
+  data: {
+    user_id: string
+  }
+}
+
+type UserInfo = {
+  id: string
+  name: string
+  email: string
+}
+
 export class FacebookApi implements LoadFacebookUserApi {
   private readonly baseUrl: string = 'https://graph.facebook.com'
 
@@ -11,14 +27,42 @@ export class FacebookApi implements LoadFacebookUserApi {
   ) {}
 
   async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
-    await this.httpClient.get({
+    return await this.getUserInfo(params.token)
+      .then(({ id, name, email }) => ({ facebookId: id, name, email }))
+      .catch(() => undefined)
+  }
+
+  private async getAppToken (): Promise<AppToken> {
+    return await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
         client_id: this.clientId,
         client_secret: this.clientSecret,
-        grant_type: 'any_grant_type'
+        grant_type: 'client_credentials'
       }
     })
-    return undefined
+  }
+
+  private async getDebugToken (clientToken: string): Promise<DebugToken> {
+    const appToken = await this.getAppToken()
+    return await this.httpClient.get({
+      url: `${this.baseUrl}/debug_token`,
+      params: {
+        access_token: appToken.access_token,
+        input_token: clientToken
+      }
+    })
+  }
+
+  private async getUserInfo (clientToken: string): Promise<UserInfo> {
+    const debugToken = await this.getDebugToken(clientToken)
+    return await this.httpClient.get({
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      url: `${this.baseUrl}/${debugToken.data.user_id}`,
+      params: {
+        fields: ['id', 'name', 'email'].join(','),
+        access_token: clientToken
+      }
+    })
   }
 }
