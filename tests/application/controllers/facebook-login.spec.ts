2f46a4917cb7b56faf20f472c859fa48194d3174
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-02-06 01:04:05 -0300

    test: ensure FacebookLoginController returns 200 if Authentication succeds

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,5 +1,6 @@
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
+import { AccessToken } from '@/domain/models'
 import { mock, MockProxy } from 'jest-mock-extended'
 
 class FacebookLoginController {
@@ -13,6 +14,15 @@ class FacebookLoginController {
       }
     }
     const result = await this.facebookAuthentication.perform({ token: httpRequest.token })
+
+    if (result instanceof AccessToken) {
+      return {
+        statusCode: 200,
+        data: {
+          accessToken: result.value
+        }
+      }
+    }
 
     return {
       statusCode: 401,
@@ -32,6 +42,7 @@ describe('FacebookLoginController', () => {
 
   beforeEach(() => {
     facebookAuth = mock<FacebookAuthentication>()
+    facebookAuth.perform.mockResolvedValue(new AccessToken('any_value'))
     sut = new FacebookLoginController(facebookAuth)
   })
 
@@ -76,6 +87,17 @@ describe('FacebookLoginController', () => {
     expect(response).toEqual({
       statusCode: 401,
       data: new AuthenticationError()
+    })
+  })
+
+  it('should return 200 if authentication succeds', async () => {
+    const response = await sut.handle({ token: 'any_token' })
+
+    expect(response).toEqual({
+      statusCode: 200,
+      data: {
+        accessToken: 'any_value'
+      }
     })
   })
 })
