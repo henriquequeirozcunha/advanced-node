28c2b824ead971085b924102c0daacfed41ee454
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-02-06 01:24:27 -0300

    refactor: move production code to their files

---
+++ src/application/errors/http.ts
@@ -0,0 +1,9 @@
+
+export class ServerError extends Error {
+  constructor (error?: Error) {
+    super('Server failed Try again soon')
+
+    this.name = 'ServerError'
+    this.stack = error?.stack
+  }
+}
