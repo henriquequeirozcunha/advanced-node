8724344c9a36be223ec3927c90b72b1901661e45
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-02-08 21:57:15 -0300

    refactor: move ValidaitonBuild to its own file

---
+++ src/application/validation/builder.ts
@@ -0,0 +1,23 @@
+import { RequiredStringValidator, Validator } from '@/application/validation'
+
+export class ValidationBuilder {
+  private constructor (
+    private readonly field: string,
+    private readonly name: string,
+    private readonly validators: Validator[] = []
+  ) {}
+
+  static of (params: { value: string, fieldName: string }): ValidationBuilder {
+    return new ValidationBuilder(params.value, params.fieldName)
+  }
+
+  required (): ValidationBuilder {
+    this.validators.push(new RequiredStringValidator(this.field, this.name))
+
+    return this
+  }
+
+  build (): Validator[] {
+    return this.validators
+  }
+}
