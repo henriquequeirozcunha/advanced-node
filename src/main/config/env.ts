67511470ff87b3e4ea4beded3c6dddfe21eeb638
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-02-11 23:02:48 -0300

    feat: ensure FacebookApi returns undefined when HttpGetClient throws

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FACEBOOK_CLIENT_ID ?? '965047794179833',
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? 'bf97ea81591852e556ce5523cda81046'
+  }
+}
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,7 +1,33 @@
-describe('', () => {
-  it('', () => {
-    const teste = 0
+import { FacebookApi } from '@/infra/apis/facebook'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
 
-    expect(teste).toBe(0)
+describe('Facebook Api integration Tests', () => {
+  it('Should return a facebook user if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret)
+
+    const fbUser = await sut.loadUser({ token: 'EAANttLL2nvkBANOpNCYrjrSxdpnkrHObc6NZCIfrY6pDXgARjd9qkZCqw9ZB2ZCfJJPCmNqI0QkNzZC57ZAVZAZBIblM5f5VWBHq3yqaGnhm7NrbJpmk2dIiywksbUeUBAZBM5DNx6Ma6U2X7ZCZA5jxjjsN41Td94l3SNhMQsT5OggEdD0sDA15TSkYnrDJZCdzoBFaCjK2pC3KzRu4Lh9kZBY6O' })
+
+    expect(fbUser).toEqual({
+      facebookId: '105616418718443',
+      name: 'Henrique Teste',
+      email: 'henrique_eujrktw_teste@tfbnw.net'
+    })
+  })
+
+  it('Should return a undefined if token is inValid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret)
+
+    const fbUser = await sut.loadUser({ token: 'invalid_token' })
+
+    expect(fbUser).toBeUndefined()
   })
 })
