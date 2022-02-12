588d2ac53ae60bd02a66fdedbfc7bac7252877da
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-02-12 00:12:03 -0300

    feat: add Facebook Login factories

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,6 @@ export const env = {
     clientId: process.env.FACEBOOK_CLIENT_ID ?? '965047794179833',
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? 'bf97ea81591852e556ce5523cda81046'
   },
-  appPort: process.env.PORT ?? 8080
+  appPort: process.env.PORT ?? 8080,
+  jwtSecret: process.env.JWT_SECRET ?? 'oijawoidja123'
 }
