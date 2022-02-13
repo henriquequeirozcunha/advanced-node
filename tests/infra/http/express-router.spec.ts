47701956f1f6e3fd28063a92a47acacdcfa27f92
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-02-13 18:41:26 -0300

    feat: ensure ExpressRouter responds with 200 and correct data

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -9,7 +9,9 @@ class ExpressRouter {
   ) {}
 
   async adapt (req: Request, res: Response): Promise<void> {
-    await this.controller.handle({ ...req.body })
+    const response = await this.controller.handle({ ...req.body })
+
+    res.status(response.statusCode).json(response.data)
   }
 }
 
@@ -23,6 +25,10 @@ describe('ExpressRouter', () => {
     req = getMockReq({ body: { any: 'any' } })
     res = getMockRes().res
     controller = mock()
+    controller.handle.mockResolvedValue({
+      statusCode: 200,
+      data: { data: 'any_data' }
+    })
     sut = new ExpressRouter(controller)
   })
 
@@ -30,6 +36,7 @@ describe('ExpressRouter', () => {
     await sut.adapt(req, res)
 
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
+    expect(controller.handle).toHaveBeenCalledTimes(1)
   })
 
   it('should call handle with empty request', async () => {
@@ -38,5 +45,15 @@ describe('ExpressRouter', () => {
     await sut.adapt(req, res)
 
     expect(controller.handle).toHaveBeenCalledWith({})
+    expect(controller.handle).toHaveBeenCalledTimes(1)
+  })
+
+  it('should respond with 200 and valid data', async () => {
+    await sut.adapt(req, res)
+
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ data: 'any_data' })
+    expect(res.json).toHaveBeenCalledTimes(1)
   })
 })
