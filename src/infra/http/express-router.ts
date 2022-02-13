f7ab691e1557358d685bef1c893d520d246e7571
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-02-13 19:12:40 -0300

    refactor: transform ExpressRouter to a high order function

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -1,13 +1,9 @@
 import { Controller } from '@/application/controllers'
-import { Request, Response } from 'express'
+import { RequestHandler } from 'express'
 
-export class ExpressRouter {
-  constructor (
-    private readonly controller: Controller
-  ) {}
-
-  async adapt (req: Request, res: Response): Promise<void> {
-    const response = await this.controller.handle({ ...req.body })
+export const adaptExpressRoute = (controller: Controller): RequestHandler => {
+  return async (req, res) => {
+    const response = await controller.handle({ ...req.body })
 
     if (response.statusCode === 200) {
       res.status(200).json(response.data)
