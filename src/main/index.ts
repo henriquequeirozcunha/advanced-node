4920d504fd2824f7071f6ef13f96851410823d52
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-02-14 00:36:48 -0300

    feat: connect to postgres database before server init

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,11 @@
 import './config/module-alias'
 import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
+import { config } from '@/infra/postgres/helpers'
 
 import 'reflect-metadata'
+import { createConnection } from 'typeorm'
 
-app.listen(env.appPort, () => console.log(`Server running at http://localhost:${env.appPort}`))
+createConnection(config).then(() =>
+  app.listen(env.appPort, () => console.log(`Server running at http://localhost:${env.appPort}`))
+).catch(console.error)
