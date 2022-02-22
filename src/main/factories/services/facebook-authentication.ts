430cdef64f3cbff670c6f952e71bdb04b213534e
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-02-21 21:43:21 -0300

    refactor: merge data layer into domain layer

--- src/main/factories/services/facebook-authentication.ts
+++ src/main/factories/services/facebook-authentication.ts
@@ -1,4 +1,4 @@
-import { FacebookAuthenticationService } from '@/data/services'
+import { FacebookAuthenticationService } from '@/domain/services'
 import { makeFacebooApi } from '@/main/factories/apis'
 import { makePgUserAccountRepo } from '@/main/factories/repos'
 import { makeJwtTokenGenerator } from '@/main/factories/crypto'
