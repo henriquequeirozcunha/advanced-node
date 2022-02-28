c4eeb6c95477ea91a755841123b7470fdb5c62fe
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-02-28 16:52:59 -0300

    refactor: move AuthenticationMiddleware to its own file

---
+++ src/application/middlewares/authentication.ts
@@ -0,0 +1,32 @@
+import { forbidden, HttpResponse, ok } from '@/application/helpers'
+import { RequiredStringValidator } from '@/application/validation'
+import { Authorize } from '@/domain/services'
+
+type HttpRequest = {
+  authorization: string
+}
+type Model = Error | { userId: string }
+
+export class AuthenticationMiddleware {
+  constructor (
+    private readonly authorize: Authorize
+  ) {}
+
+  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
+    try {
+      if (!this.validate({ authorization })) return forbidden()
+
+      const userId = await this.authorize({ token: authorization })
+
+      return ok({ userId })
+    } catch {
+      return forbidden()
+    }
+  }
+
+  private validate ({ authorization }: HttpRequest): boolean {
+    const error = new RequiredStringValidator(authorization, 'authorization').validate()
+
+    return error === undefined
+  }
+}
