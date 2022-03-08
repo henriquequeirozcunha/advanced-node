7b108a2f568a7196863cc736cda5f74be658fe18
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-03-07 21:58:15 -0300

    feat: ensure ChangeProfilePicture calls SaveUserPictureRepository with correct input

---
+++ src/domain/contracts/repos/user-profile.ts
@@ -0,0 +1,8 @@
+
+export interface SaveUserPictureRepository {
+  savePicture: (input: SaveUserPictureRepository.Input) => Promise<void>
+}
+
+export namespace SaveUserPictureRepository {
+  export type Input = { pictureUrl: string }
+}
--- tests/domain/services/change-profile-picture.spec.ts
+++ tests/domain/services/change-profile-picture.spec.ts
@@ -1,24 +1,29 @@
-import { mock, MockProxy } from 'jest-mock-extended'
 import { UploadFile, UUIDGenerator } from '@/domain/contracts/gateways'
 import { ChangeProfilePicture, setupChangeProfilePicture } from '@/domain/services'
+import { SaveUserPictureRepository } from '@/domain/contracts/repos'
+
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('ChangeProfilePicture', () => {
   let uuid: string
   let file: Buffer | undefined
   let fileStorage: MockProxy<UploadFile>
   let crypto: MockProxy<UUIDGenerator>
+  let userProfileRepo: MockProxy<SaveUserPictureRepository>
   let sut: ChangeProfilePicture
 
   beforeAll(() => {
     uuid = 'any_unique_id'
     file = Buffer.from('any_buffer')
-    fileStorage = mock<UploadFile>()
-    crypto = mock<UUIDGenerator>()
+    fileStorage = mock()
+    fileStorage.upload.mockResolvedValue('any_url')
+    crypto = mock()
     crypto.uuid.mockReturnValue(uuid)
+    userProfileRepo = mock()
   })
 
   beforeEach(() => {
-    sut = setupChangeProfilePicture(fileStorage, crypto)
+    sut = setupChangeProfilePicture(fileStorage, crypto, userProfileRepo)
   })
 
   it('should call UploadFile with correct input', async () => {
@@ -32,5 +37,12 @@ describe('ChangeProfilePicture', () => {
     await sut({ userId: 'any_user_id', file: undefined })
 
     expect(fileStorage.upload).not.toHaveBeenCalled()
+  })
+
+  it('should call SaveUserPictureRepository with correct input', async () => {
+    await sut({ userId: 'any_user_id', file })
+
+    expect(userProfileRepo.savePicture).toHaveBeenCalledWith({ pictureUrl: 'any_url' })
+    expect(userProfileRepo.savePicture).toHaveBeenCalledTimes(1)
   })
 })
