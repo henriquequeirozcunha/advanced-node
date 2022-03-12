e58fdd5a2b0aded107c40caf621dcb889943f61e
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-03-12 11:08:56 -0300

    feat: ensure UUIDHandler returns correct value

---
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -0,0 +1,31 @@
+import { UUIDGenerator } from '@/domain/contracts/gateways'
+import { mocked } from 'ts-jest/utils'
+import { v4 } from 'uuid'
+
+class UUIDHandler {
+  uuid ({ key }: UUIDGenerator.Input): UUIDGenerator.Output {
+    return `${key}_${v4()}`
+  }
+}
+
+jest.mock('uuid')
+
+describe('UUIDHandler', () => {
+  it('should call uuid.v4', () => {
+    const sut = new UUIDHandler()
+
+    sut.uuid({ key: 'any_key' })
+
+    expect(v4).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return correct uuid', () => {
+    mocked(v4).mockReturnValueOnce('any_uuid')
+
+    const sut = new UUIDHandler()
+
+    const uuid = sut.uuid({ key: 'any_key' })
+
+    expect(uuid).toBe('any_key_any_uuid')
+  })
+})
