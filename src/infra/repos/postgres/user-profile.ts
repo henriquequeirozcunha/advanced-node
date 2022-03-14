97c6f36c6613e2dff2847c7e34652048e5757895
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-03-13 23:09:41 -0300

    feat: ensure PgUserProfileRepository loads an user

--- src/infra/repos/postgres/user-profile.ts
+++ src/infra/repos/postgres/user-profile.ts
@@ -1,12 +1,20 @@
-import { SaveUserPictureRepository } from '@/domain/contracts/repos'
+import { LoadUserProfileRepository, SaveUserPictureRepository } from '@/domain/contracts/repos'
 import { PgUser } from '@/infra/repos/postgres/entities'
 
 import { getRepository } from 'typeorm'
 
-export class PgUserProfileRepository implements SaveUserPictureRepository {
+export class PgUserProfileRepository implements SaveUserPictureRepository, LoadUserProfileRepository {
   async savePicture ({ id, pictureUrl, initials }: SaveUserPictureRepository.Input): Promise<void> {
     const pgUserRepo = getRepository(PgUser)
 
     await pgUserRepo.update({ id: parseInt(id) }, { pictureUrl, initials })
+  }
+
+  async load ({ id }: LoadUserProfileRepository.Input): Promise<LoadUserProfileRepository.Output> {
+    const pgUserRepo = getRepository(PgUser)
+
+    const pgUser = await pgUserRepo.findOne({ id: parseInt(id) })
+
+    if (pgUser !== undefined) return { name: pgUser.name }
   }
 }
