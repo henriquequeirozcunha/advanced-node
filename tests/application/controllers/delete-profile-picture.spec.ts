270becd7a412583da6bd348b5714a4232e81a847
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-03-22 22:47:13 -0300

    feat: ensure DeleteProfilePictureController returns 204

--- tests/application/controllers/delete-profile-picture.spec.ts
+++ tests/application/controllers/delete-profile-picture.spec.ts
@@ -1,12 +1,15 @@
 import { ChangeProfilePicture } from '@/domain/services'
+import { HttpResponse, noContent } from '@/application/helpers'
 
 type HttpRequest = { userId: string }
 
 class DeleteProfilePictureController {
   constructor (private readonly changeProfilePicture: ChangeProfilePicture) {}
 
-  async handle ({ userId }: HttpRequest): Promise<void> {
+  async handle ({ userId }: HttpRequest): Promise<HttpResponse> {
     await this.changeProfilePicture({ userId })
+
+    return noContent()
   }
 }
 
@@ -27,5 +30,14 @@ describe('DeleteProfilePictureController', () => {
 
     expect(changeProfilePicture).toHaveBeenCalledWith({ userId: 'any_user_id', file: undefined })
     expect(changeProfilePicture).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 204', async () => {
+    const httpResponse = await sut.handle({ userId: 'any_user_id' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 204,
+      data: null
+    })
   })
 })
