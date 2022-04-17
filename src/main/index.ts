ae6d5d1178c7ab0ccf6c8637cbfae54336c147ae
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-04-16 23:28:38 -0300

    fix: connection problems

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,15 @@
 import './config/module-alias'
-import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
 
 import 'reflect-metadata'
-import { createConnection } from 'typeorm'
+import { createConnection, getConnectionOptions } from 'typeorm'
 
-createConnection().then(() =>
+getConnectionOptions().then(async options => {
+  const root = process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'
+  const entities = [`${root}/infra/repos/postgres/entities/index.{js,ts}`]
+  await createConnection({ ...options, entities })
+
+  const { app } = await import('@/main/config/app')
+
   app.listen(env.appPort, () => console.log(`Server running at http://localhost:${env.appPort}`))
-).catch(console.error)
+}).catch(console.error)
