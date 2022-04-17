ee3b0714f879aaac7a1c07b8a158b18d6bfd5abf
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-04-16 23:07:35 -0300

    feat: ensure PUT /users/picture returns 200 on success

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-import { adaptExpressRoute as adapt } from '@/main/adapters'
+import { adaptExpressRoute as adapt, adaptMulter as upload } from '@/main/adapters'
 import { makeSaveProfilePictureController } from '@/main/factories/controllers'
 
 import { auth } from '@/main/middlewares'
 
 export default (router: Router): void => {
   router.delete('/users/picture', auth, adapt(makeSaveProfilePictureController()))
-  router.put('/users/picture', auth)
+  router.put('/users/picture', auth, upload, adapt(makeSaveProfilePictureController()))
 }
