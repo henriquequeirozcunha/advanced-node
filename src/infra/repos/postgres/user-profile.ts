0bfff8a902f9bc369fb4890042242d841177e39b
Henrique Cunha <henrique.cunha@corpus.inf.br>
2022-04-17 19:16:03 -0300

    refactor: apply PgConnection

--- src/infra/repos/postgres/user-profile.ts
+++ src/infra/repos/postgres/user-profile.ts
@@ -1,17 +1,16 @@
 import { LoadUserProfileRepository, SaveUserPictureRepository } from '@/domain/contracts/repos'
 import { PgUser } from '@/infra/repos/postgres/entities'
+import { PgRepository } from '@/infra/repos/postgres/repository'
 
-import { getRepository } from 'typeorm'
-
-export class PgUserProfileRepository implements SaveUserPictureRepository, LoadUserProfileRepository {
+export class PgUserProfileRepository extends PgRepository implements SaveUserPictureRepository, LoadUserProfileRepository {
   async savePicture ({ id, pictureUrl, initials }: SaveUserPictureRepository.Input): Promise<void> {
-    const pgUserRepo = getRepository(PgUser)
+    const pgUserRepo = this.getRepository(PgUser)
 
     await pgUserRepo.update({ id: parseInt(id) }, { pictureUrl, initials })
   }
 
   async load ({ id }: LoadUserProfileRepository.Input): Promise<LoadUserProfileRepository.Output> {
-    const pgUserRepo = getRepository(PgUser)
+    const pgUserRepo = this.getRepository(PgUser)
 
     const pgUser = await pgUserRepo.findOne({ id: parseInt(id) })
 
--- tests/infra/repos/postgres/user-account.spec.ts
+++ tests/infra/repos/postgres/user-account.spec.ts
@@ -1,12 +1,15 @@
 import { PgUser } from '@/infra/repos/postgres/entities'
 import { makeFakeDb } from '@/tests/infra/repos/postgres/mocks'
 import { PgUserAccountRepository } from '@/infra/repos/postgres'
+import { PgRepository } from '@/infra/repos/postgres/repository'
+import { PgConnection } from '@/infra/repos/postgres/helpers'
 
 import { IBackup } from 'pg-mem'
-import { getConnection, getRepository, Repository } from 'typeorm'
+import { Repository } from 'typeorm'
 
 describe('PgUserAccountRepository', () => {
   let sut: PgUserAccountRepository
+  let connection: PgConnection
   let pgUserRepo: Repository<PgUser>
   let backup: IBackup
 
@@ -16,13 +19,18 @@ describe('PgUserAccountRepository', () => {
   })
 
   beforeAll(async () => {
+    connection = PgConnection.getInstance()
     const db = await makeFakeDb([PgUser])
     backup = db.backup()
-    pgUserRepo = getRepository(PgUser)
+    pgUserRepo = connection.getRepository(PgUser)
   })
 
   afterAll(async () => {
-    await getConnection().close()
+    await connection.disconnect()
+  })
+
+  it('should extend PgRepository', async () => {
+    expect(sut).toBeInstanceOf(PgRepository)
   })
 
   describe('load', () => {
